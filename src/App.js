de1e8917141ae4eb605a6758a89e4f5c5e232533
prayag <prayagkothiya111@gmail.com>
2022-02-08 16:38:50 +0530

    first app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./App.css"
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber, decNumber} from "./action/index";
+
+const App = () => {
+  const myState = useSelector((state) => state.changeTheNumber)
+  const dispatch = useDispatch();
+  return (
+
+      <>
+        <div className="container">
+          <h1>Increment/Decrement Counter</h1>
+          <h4>Using React and Redux</h4>
+
+          <div className="quantity">
+            <button  className="quantity__minus" title='Decrement' onClick={ () => dispatch(decNumber()) }><span> - </span></button>
+            <input  name="quantity" type="text" className= "quantity__input" value={myState} />
+            <button className="quantity__plus"  title='Increment' onClick={ () => dispatch(incNumber()) }><span> + </span></button>
+          </div>
+        </div>
+      </>
+  )
+};
+
+export default App;
---
+++ src/reducers/upDown.js
@@ -0,0 +1,12 @@
+const initialSatate = 111;
+
+const changeTheNumber = (state = initialSatate, action) => {
+    switch (action.type) {
+        case "INCREMENT" : return state + 1;
+        case "DECREMENT" : return state - 1;
+        default : return state;
+    }
+
+}
+
+export default changeTheNumber;
